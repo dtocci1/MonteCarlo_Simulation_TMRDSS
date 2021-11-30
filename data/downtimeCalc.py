697720fc28aaa46218cb70cb901e8a76f6c772b7
Dylan Tocci <dtocci1@umassd.edu>
2021-11-30 12:09:09 -0500

    quick code for calc stats about downtime, needs bugfix

---
+++ data/downtimeCalc.py
@@ -0,0 +1,43 @@
+# Chicken scratch code to get stats about up/down time
+
+def main():
+    with open('data/stor_state_tmr.csv','r') as f:
+        data = f.readlines()
+        new_data=[]
+        for ind in data:
+            new_data.append(int(ind.replace('\n','')))
+        #print(new_data)
+
+    d_count = 0
+    u_count = 0
+    downtime = []
+    uptime = []
+    for i in range(len(new_data)):
+        if new_data[i] == 0:
+            uptime.append(u_count)
+            u_count = 0
+            d_count += 1
+
+        if new_data[i] == 1:
+            downtime.append(d_count)
+            d_count = 0
+            u_count += 1
+
+    new_downtime = []
+    new_uptime = []
+
+    for i in range(len(downtime)):
+        if downtime[i] != 0:
+            new_downtime.append(downtime[i])
+
+    for i in range(len(uptime)):
+        if uptime[i] != 0:
+            new_uptime.append(uptime[i])
+
+    print(new_uptime)
+    print(new_downtime)
+
+    print(sum(new_uptime))
+    print(sum(new_downtime))
+if __name__ == '__main__':
+    main()
